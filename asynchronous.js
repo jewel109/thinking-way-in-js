f01341f1cfeb4ba18ca33e5072868278e47abd92
jewel109 <jewel140990@gmail.com>
2021-04-13 09:47:07 +0600

    new

--- asynchronous.js
+++ asynchronous.js
@@ -1,14 +1,14 @@
 const processOrder = () => {
-    console.log(`processing order for customer 1`);
-    var currentTime = new Date().getTime();
-    
-    while (currentTime + 3000 >= new Date().getTime()) {
-        
-    };
+	console.log(`processing order for customer 1`);
+	var currentTime = new Date().getTime();
 
-    console.log(`order processed for customer 1`)
-}
+	setTimeout(() => {
+		console.log(`cooking food is completed`);
+	}, 3000);
+
+	console.log(`order processed for customer 1`);
+};
 
 console.log(`take order from customer 1`);
 processOrder();
-console.log(`completed order for customer 1`)
+console.log(`completed order for customer 1`);
