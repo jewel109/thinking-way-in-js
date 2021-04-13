d4a2c89bfa037337f62808016975444a1c682f83
jewel109 <jewel140990@gmail.com>
2021-04-13 09:41:09 +0600

    file added

---
+++ asynchronous.js
@@ -0,0 +1,14 @@
+const processOrder = () => {
+    console.log(`processing order for customer 1`);
+    var currentTime = new Date().getTime();
+    
+    while (currentTime + 3000 >= new Date().getTime()) {
+        
+    };
+
+    console.log(`order processed for customer 1`)
+}
+
+console.log(`take order from customer 1`);
+processOrder();
+console.log(`completed order for customer 1`)
